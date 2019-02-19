06b905c7ff80fd7c692273a19b112e18d814db9e
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-02-18 22:21:40 -0500

    update for SMAPI 3.0, minor refactoring

--- walkthroughtrellis/ModEntry.cs
+++ walkthroughtrellis/ModEntry.cs
@@ -1,55 +1,56 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using StardewValley;
-using StardewValley.TerrainFeatures;
+﻿using System.Linq;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.TerrainFeatures;
 
-namespace walkthroughtrellis
+namespace WalkThroughTrellis
 {
-    //Entry
+    /// <summary>The mod entry class loaded by SMAPI.</summary>
     public class ModEntry : Mod, IAssetEditor
     {
-        //Entry Method
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            //implementing the event
-            PlayerEvents.Warped += PlayerEvents_Warped;
-            //was current location and now that's different?
+            helper.Events.Player.Warped += OnWarped;
         }
-        //location change event
-        public void PlayerEvents_Warped(object sender, EventArgs e)
+
+        /// <summary>Raised after a player warps to a new location.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        public void OnWarped(object sender, WarpedEventArgs e)
         {
+            if (!e.IsLocalPlayer)
+                return;
+
             /*Telling it for every HoeDirt type that is dirt in the locations gathered (Thanks PathosChild <3)
             with the given parameters, make the raised seed setting for the crops false. 
             */
-            foreach (HoeDirt dirt in Game1.currentLocation.terrainFeatures.Values.OfType<HoeDirt>().Where(dirt => dirt.crop != null).ToArray())
-            {
+            foreach (HoeDirt dirt in Game1.currentLocation.terrainFeatures.Values.OfType<HoeDirt>().Where(dirt => dirt.crop != null))
                 dirt.crop.raisedSeeds.Value = false;
-                //was dirt.crop.raisedSeeds?
-            }
         }
-        //Telling smapi that we can edit the assetinfo of the crops XNB in the data folder of SDV
+
+        /// <summary>Get whether this instance can edit the given asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
         public bool CanEdit<T>(IAssetInfo asset)
         {
+            // tell SMAPI we can edit the crops XNB asset
             return asset.AssetNameEquals(@"Data\Crops");
         }
-        //Telling it what we want to actually edit in SDV's crops XNB and how exactly we want to do that.
+
+        /// <summary>Edit a matched asset.</summary>
+        /// <param name="asset">A helper which encapsulates metadata about an asset and enables changes to it.</param>
         public void Edit<T>(IAssetData asset)
         {
-            asset
-            .AsDictionary<int, string>()
-            .Set((id, data) =>
+            // tell SMAPI what we want to change in the crops XNB asset
+            var data = asset.AsDictionary<int, string>().Data;
+            foreach (int id in data.Keys.ToArray())
             {
-                //splits the fields in the crops XNB via the '/' which is how values are seperated in the xnb
-                string[] fields = data.Split('/');
-                //setting the field for raised seeds to false (This is for all crops, maybe in the future I'll be more explicit)
-                fields[7] = "false";
-                //stitching the fields in the XNB file back together for use.
-                return string.Join("/", fields);
-            });
+                string[] fields = data[id].Split('/'); // split fields in the crops XNB via the '/' which is how they're separated in the xnb
+                fields[7] = "false"; // set 'raised seeds' field to false (This is for all crops, maybe in the future I'll be more explicit)
+                data[id] = string.Join("/", fields); // stitch fields back into the XNB file's format
+            }
         }
     }
 }
